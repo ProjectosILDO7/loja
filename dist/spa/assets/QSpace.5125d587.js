eb0a3273d6b15d52d9686407e1696f5e56d6bcc7
ProjectosILDO7 <marques2cuema@gmail.com>
2024-01-21 07:55:44 +0100

    Build update in quasar.config

---
+++ dist/spa/assets/QSpace.5125d587.js
@@ -0,0 +1,1 @@
+import{h as e,k as a}from"./index.45227f5a.js";const p=e("div",{class:"q-space"});var c=a({name:"QSpace",setup(){return()=>p}});export{c as Q};
---
+++ dist/spa/assets/use-quasar.4bd664ae.js
@@ -0,0 +1,1 @@
+import{i as a,aC as r}from"./index.45227f5a.js";function u(){return a(r)}export{u};
